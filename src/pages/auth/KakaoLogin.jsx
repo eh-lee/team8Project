0fd0826f7ded86030c5de4af541c4c94fd9270ae
eh-lee <fornewstack@daum.net>
2023-04-03 21:09:38 +0900

    [Test] 카카오로그인 인가 코드 전송 커밋

--- src/pages/auth/KakaoLogin.jsx
+++ src/pages/auth/KakaoLogin.jsx
@@ -5,18 +5,15 @@ import axios from "axios";
 const KakaoLogin = () => {
   // 통신중인 페이지 => 꾸밈 / isLoading, isError
   const location = useLocation();
-  const KaKaocode = location.search.split("=")[1];
+  const KaKaocode = { code: location.search.split("=")[1] };
   // console.log(location.search.split('=')[1])
 
   useEffect(() => {
     async function postKaKaoCode() {
       const response = await axios.post(
-        "http://sth/api/user/kakaoLogin",
+        "http://54.180.30.108:3002/user/kakaoLogin/",
         KaKaocode
       );
-      // .then((response.request.statusCode === 200)=>{
-      // navigate('/');
-      // })
     }
     postKaKaoCode();
   }, []);
