09119bb67b83cd3ae4a691949758753617702050
yeooong <yeong.kim9127@gmail.com>
2023-04-05 13:44:11 +0900

    [Refactor] kakao 로그인 api 수정 및 코드 리팩토링

---
+++ src/api/login.js
@@ -0,0 +1,6 @@
+const {
+    REACT_APP_KAKAO_REST_API_KEY,
+    REACT_APP_KAKAO_REDIRECT_URI,
+} = process.env;
+
+export const KAKAO_LOGIN_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REACT_APP_KAKAO_REST_API_KEY}&redirect_uri=${REACT_APP_KAKAO_REDIRECT_URI}&response_type=code`;
