d1f55306f4db30ee02b131cd5b28b3c8461e35d2
eh-lee <fornewstack@daum.net>
2023-04-05 22:33:17 +0900

    [Fix] #2 WebView Scroll Issue Resolved Commit

--- src/layout/MobileLayout.jsx
+++ src/layout/MobileLayout.jsx
@@ -30,9 +30,15 @@ const StMobileLayout = styled.div`
   height: 100vh;
   /* 컨텐츠가 없는 경우에도 화면이 잘리지 않도록 100vh */
   position: relative;
+  overflow-y: scroll;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  /* 스크롤 */
   margin: 0 auto;
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.3); /* 그림자 추가 */
   background-color: white;
+
   @media screen and (min-width: 520px) {
     /* min-width로 반응형 width 조절 */
     /* 520px: 카카오 로그인 max-width와 통일 */
