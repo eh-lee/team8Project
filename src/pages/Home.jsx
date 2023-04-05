428471d8ec0be3c3da6efebbbab67165cd14fb10
eh-lee <fornewstack@daum.net>
2023-04-05 22:54:29 +0900

    [Refactor] #8 Home.jsx 테스트 코드 제거 커밋

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,38 +11,6 @@ const Home = () => {
           // title={title} nickname={nickname} content={content}
           title="타이틀을 막 바꿀 수 있지요하하하하하"
           nickname="하하하하하하하하하하하하하하하"
-          content="시작 ! 컨텐츠 길어지면 어떻게 될까요 컨텐츠 길어지면 어떻게 될까요 시작 ! 컨텐츠 길어지면 어떻게 될까요 컨텐츠 길어지면 어떻게 될까요시작 ! 컨텐츠 길어지면 어떻게 될까요 컨텐츠 길어지면 어떻게 될까요시작 ! 컨텐츠 길어지면 어떻게 될까요 컨텐츠 길어지면 어떻게 될까요시작 ! 컨텐츠 길어지면 어떻게 될까요 컨텐츠 길어지면 어떻게 될까요시작 ! 컨텐츠 길어지면 어떻게 될까요 컨텐츠 길어지면 어떻게 될까요시작 ! 컨텐츠 길어지면 어떻게 될까요 컨텐츠 길어지면 어떻게 될까요시작 ! 컨텐츠 길어지면 어떻게 될까요 컨텐츠 길어지면 어떻게 될까요시작 ! 컨텐츠 길어지면 어떻게 될까요 컨텐츠 길어지면 어떻게 될까요시작 ! 컨텐츠 길어지면 어떻게 될까요 컨텐츠 길어지면 어떻게 될까요시작 ! 컨텐츠 길어지면 어떻게 될까요 컨텐츠 길어지면 어떻게 될까요시작 ! 컨텐츠 길어지면 어떻게 될까요 컨텐츠 길어지면 어떻게 될까요시작 ! 컨텐츠 길어지면 어떻게 될까요 컨텐츠 길어지면 어떻게 될까요시작 ! 컨텐츠 길어지면 어떻게 될까요 컨텐츠 길어지면 어떻게 될까요시작 ! 컨텐츠 길어지면 어떻게 될까요 컨텐츠 길어지면 어떻게 될까요시작 ! 컨텐츠 길어지면 어떻게 될까요 컨텐츠 길어지면 어떻게 될까요시작 ! 컨텐츠 길어지면 어떻게 될까요 컨텐츠 길어지면 어떻게 될까요시작 ! 컨텐츠 길어지면 어떻게 될까요 컨텐츠 길어지면 어떻게 될까요시작 ! 컨텐츠 길어지면 어떻게 될까요 컨텐츠 길어지면 어떻게 될까요시작 ! 컨텐츠 길어지면 어떻게 될까요 컨텐츠 길어지면 어떻게 될까요시작 ! 컨텐츠 길어지면 어떻게 될까요 컨텐츠 길어지면 어떻게 될까요시작 ! 컨텐츠 길어지면 어떻게 될까요 컨텐츠 길어지면 어떻게 될까요시작 ! 컨텐츠 길어지면 어떻게 될까요 컨텐츠 길어지면 어떻게 될까요시작 ! 컨텐츠 길어지면 어떻게 될까요 컨텐츠 길어지면 어떻게 될까요시작 ! 컨텐츠 길어지면 어떻게 될까요 컨텐츠 길어지면 어떻게 될까요"
-          viewCount="viewCount"
-          commentCount="commentCount"
-        />
-        <PostCard
-          // title={title} nickname={nickname} content={content}
-          title="타이틀을 막 바꿀 수 있지요하하하하하"
-          nickname="하하하하하하하하하하하하하하하"
-          content="시작 ! 컨텐츠 길어지면 어떻게 될까요"
-          viewCount="viewCount"
-          commentCount="commentCount"
-        />
-        <PostCard
-          // title={title} nickname={nickname} content={content}
-          title="타이틀을 막 바꿀 수 있지요하하하하하"
-          nickname="하하하하하하하하하하하하하하하"
-          content="시작 ! 컨텐츠 길어지면 어떻게 될까요"
-          viewCount="viewCount"
-          commentCount="commentCount"
-        />
-        <PostCard
-          // title={title} nickname={nickname} content={content}
-          title="타이틀을 막 바꿀 수 있지요하하하하하"
-          nickname="하하하하하하하하하하하하하하하"
-          content="시작 ! 컨텐츠 길어지면 어떻게 될까요"
-          viewCount="viewCount"
-          commentCount="commentCount"
-        />
-        <PostCard
-          // title={title} nickname={nickname} content={content}
-          title="타이틀을 막 바꿀 수 있지요하하하하하"
-          nickname="하하하하하하하하하하하하하하하"
           content="시작 ! 컨텐츠 길어지면 어떻게 될까요"
           viewCount="viewCount"
           commentCount="commentCount"
