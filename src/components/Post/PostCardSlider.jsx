db8716feecc6c2522843cc8c618d9f04ebb71d34
yeooong <yeong.kim9127@gmail.com>
2023-04-08 14:47:05 +0900

    [Test]PostCard 인피니트 캐러셀 테스트

---
+++ src/components/Post/PostCardSlider.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+const PostCardSlider = () => {
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PostCardSlider
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,35 +3,37 @@ import PostCard from "../components/Post/PostCard";
 import Footer from "../components/footer/Footer";
 import MobileLayout from "../layout/MobileLayout";
 import { instance } from "../api/axios";
+import PostCardSlider from "../components/Post/PostCardSlider";
 
 const Home = () => {
 
-  const [page, setPage] = useState(1);
+  // const [page, setPage] = useState(1);
 
-  const [postCards, setPostCards] = useState([]);
-  const [hotPostCards, setHotPostCards] = useState([]);
+  // const [postCards, setPostCards] = useState([]);
+  // const [hotPostCards, setHotPostCards] = useState([]);
 
-  useEffect(()=>{
-    const getHotPost = async() => {
-      const response = await instance.get('/postCards/hotPostCard')
-      setHotPostCards(response.data.postCards)
-    }
+  // useEffect(()=>{
+  //   const getHotPost = async() => {
+  //     const response = await instance.get('/postCards/hotPostCard')
+  //     setHotPostCards(response.data.postCards)
+  //   }
 
-    const getPost = async() => {
-      const response = await instance.get(`/postCards?maincategory=전체&category=전체&splitNumber=10&splitPageNumber=${page}`)
-      setPostCards(response.data.postCards)
-    }
+  //   const getPost = async() => {
+  //     const response = await instance.get(`/postCards?maincategory=전체&category=전체&splitNumber=10&splitPageNumber=${page}`)
+  //     setPostCards(response.data.postCards)
+  //   }
     
-    getHotPost();
-    getPost();
-  }, [])
+  //   getHotPost();
+  //   getPost();
+  // }, [])
 
-  console.log('postCards', postCards);
+  // console.log('postCards', postCards);
 
   return (
     <>
       <MobileLayout>
-        <h1>Hot 게시물</h1>
+        <PostCardSlider />
+        {/* <h1>Hot 게시물</h1>
         {
           hotPostCards?.map((hotPostCard) => {
             return (
@@ -56,7 +58,7 @@ const Home = () => {
             )
           })
         }
-        <Footer />
+        <Footer /> */}
       </MobileLayout>
     </>
   )
