a8fca479a0b5c25393d8f35a55f31ad518943912
eh-lee <fornewstack@daum.net>
2023-04-10 16:36:52 +0900

    [Feat] 메인 카테고리 필터링 기능 추가, Board.jsx 하위 페이지 통합, 메인 카테고리 포커스 옵션 추가 커밋

--- src/pages/board/Board.jsx
+++ src/pages/board/Board.jsx
@@ -1,6 +1,4 @@
-import React from "react";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { instance } from "../../api/axios";
 import Footer from "../../components/footer/Footer";
@@ -8,40 +6,79 @@ import PostCard from "../../components/post/PostCard";
 import MobileLayout from "../../layout/MobileLayout";
 
 const Board = () => {
-  const navi = useNavigate();
-  // *============ req.heaeders에 token 있을 때, ==========*
-  // const token = cookies.get("access_token");
-  // console.log(token);
+  const [mainCategory, setMainCategory] = useState("유머");
+  const [category, setCategory] = useState("전체");
+  const [page, setPage] = useState(1);
+  const [data, setData] = useState([]);
 
-  // useEffect(() => {
-  //   const response = instance.get("/sth",
-  //   {
-  //     headers : {
-  //       Authorization: `${token}`
-  //     }
-  //   });
-  //   console.log(response);
-  // }, []);
-  // *============ req.heaeders에 token 있을 때, ==========*
+  const categories = [
+    "전체",
+    "패션/뷰티",
+    "맛집/요리/음식",
+    "경제/재테크",
+    "취미/운동",
+    "스포츠",
+    "여행",
+    "결혼",
+    "게임",
+    "반려동물",
+    "가족",
+    "취업/자격증",
+    "일상",
+    "기타",
+  ];
 
-  // 아직 dummy 없넹..
+  const handleMainCategoryClick = (mainCategory) => {
+    setMainCategory(mainCategory);
+    setPage(1);
+  };
+
+  const handleCategoryClick = (category) => {
+    setCategory(category);
+    setPage(1);
+  };
 
   useEffect(() => {
-    const sth = async () => {
-      // const response = await instance.get("/postCards?maincategory=전체");
-      const response = await instance.get(
-        "/postCards?maincategory=전체&category=전체&splitNumber=4&splitPageNumber=3"
-        // ----> 1페이지당 4장씩 나눴을 때, 3페이지에 해당하는 postCards
-        // "/postCards?maincategory=전체&category=전체&splitNumber=4"
-      );
-      console.log("WholeBoard---->", response.data.postCards);
-      // return response.data.postCards;
+    const fetchData = async () => {
+      try {
+        const response = await instance.get(
+          `/postCards?maincategory=${mainCategory}&category=${category}&splitNumber=4&splitPageNumber=${page}`
+        );
+        setData(response.data.postCards);
+
+        //infinite할 때
+        // if category전환되면, ...prev 초기화되어서 중복되어 쌓이지 않게 && same key issue handling
+        // setData((prev) => [...prev, ...response.data.postCards]); //요놈
+      } catch (error) {
+        console.error(error);
+      }
     };
-    sth();
+    fetchData();
+  }, [mainCategory, category, page]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      //try1
+      // const ScrollHeight = document.documentElement.scrollHeight; //스크롤 하지 않았을 때의 전체 높이
+      // const ScrollTop = document.documentElement.scrollTop; //y축 방향으로 스크롤한 거리=스크롤되어 올라간 만큼의 높이
+      // const clientHeight = document.documentElement.clientHeight; // 눈에 보이는 만큼의 높이
+      // const isBottom = clientHeight + ScrollTop + 1 >= ScrollHeight + "500px";
+
+      //try2
+      const isBottom =
+        window.innerHeight + window.scrollY >= document.body.offsetHeight;
+      // + "500px";
+      if (isBottom) {
+        setPage((prevPage) => prevPage + 1);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
 
-  // PostCards-> title, nickname, content, viewCount, commentCount
-  // res.data->  title, nickname, desc,
+  //세부 카테고리를 선택해도 전체 카테고리가 호출됨.
 
   return (
     <MobileLayout>
@@ -49,48 +86,82 @@ const Board = () => {
         <BoardHeader>
           <BoardHeaderSub>
             <BoardTitle>훈수게시판</BoardTitle>
+
             <Row>
-              {/* if문으로 색상 & axios | with state bool */}
-              <BoardCategory onClick={() => navi("/board/humour")}>
+              <MainCategory
+                isActive={mainCategory === "유머"}
+                onClick={() => handleMainCategoryClick("유머")}
+              >
                 유머
-              </BoardCategory>
-              <BoardCategory onClick={() => navi("/board/serious")}>
+              </MainCategory>
+
+              <MainCategory
+                isActive={mainCategory === "진지"}
+                onClick={() => handleMainCategoryClick("진지")}
+              >
                 진지
-              </BoardCategory>
+              </MainCategory>
             </Row>
+            <CategorySlider>
+              {categories.map((item, index) => (
+                <button
+                  key={index}
+                  onClick={() => handleCategoryClick(item)}
+                  style={{ marginLeft: index === 0 ? "1.25rem" : "0.5rem" }}
+                >
+                  {item}
+                </button>
+              ))}
+            </CategorySlider>
           </BoardHeaderSub>
         </BoardHeader>
-        {/*  */}
-        <PostCard
-        // title={title} nickname={nickname} content={desc}
-        />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        {/*  */}
+        {data?.map((item) => (
+          <PostCard
+            key={item.postIdx}
+            title={item.title}
+            nickname={item.nickname}
+            content={item.desc}
+            viewCount={item.postViewCount}
+            commentCount={item.commentCount}
+          />
+        ))}
       </PageWithFooterWrapper>
       <Footer />
     </MobileLayout>
   );
 };
 
-const PageWithFooterWrapper = styled.div`
-  margin: 8rem 0 15rem 0;
-  /* margin-top 조정함 */
+const CategorySlider = styled.div`
+  padding: 1rem 0;
+  width: 400px;
+  background-color: white;
+  overflow-x: scroll;
+  white-space: nowrap;
+  /* margin-left: 1rem; */
+
+  & > button {
+    display: inline-block;
+    background-color: white;
+    border: 0.1rem solid rgb(120, 120, 120);
+    margin-right: 0.5rem;
+    border-radius: 1.75rem;
+    padding: 0.25rem 1rem;
+  }
+
+  & > button:focus {
+    background-color: rgb(120, 120, 120);
+    color: white;
+    outline: none;
+  }
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
-// const BoardLink = styled(Link)`
-//   color: rgb(70, 70, 70);
-//   text-decoration: none;
-
-//   &:hover {
-//     color: rgb(180, 180, 180);
-//     cursor: pointer;
-//   }
-// `;
+const PageWithFooterWrapper = styled.div`
+  margin: 11rem 0 15rem 0;
+`;
 
 const Row = styled.div`
   display: flex;
@@ -112,246 +183,38 @@ const BoardHeader = styled.div`
 const BoardHeaderSub = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 2rem;
   font-weight: bold;
   justify-content: space-evenly;
 `;
 
-const BoardCategory = styled.div`
+const MainCategory = styled.div`
   display: flex;
   justify-content: center;
   width: 100%;
   padding-bottom: 0.75rem;
-  border-bottom: 0.2rem solid rgb(70, 70, 70);
-  color: rgb(70, 70, 70);
   text-decoration: none;
 
+  color: ${(props) =>
+    props.isActive ? "rgb(70, 70, 70)" : "rgb(180, 180, 180)"};
+  border-bottom: ${(props) =>
+    props.isActive
+      ? "0.2rem solid rgb(70, 70, 70)"
+      : "0.1rem solid rgb(180, 180, 180)"};
+
   &:hover {
-    color: rgb(180, 180, 180);
     cursor: pointer;
-    border-bottom: 0.1rem solid rgb(180, 180, 180);
+  }
+
+  &:active {
+    color: rgb(70, 70, 70);
+    border-bottom: 0.2rem solid rgb(70, 70, 70);
   }
 `;
 
 const BoardTitle = styled.div`
+  padding-bottom: 2rem;
   display: flex;
   justify-content: center;
 `;
 
 export default Board;
-
-//=================Refactor================
-
-// import React, { useState, useEffect } from "react";
-// import { useNavigate } from "react-router-dom";
-// import styled from "styled-components";
-// import { instance } from "../../api/axios";
-// import Footer from "../../components/footer/Footer";
-// import PostCard from "../../components/post/PostCard";
-// import MobileLayout from "../../layout/MobileLayout";
-
-// const HumourBoard = () => {
-//   const navi = useNavigate();
-//   // const [humour, setHumour] = useState([]);
-//   const [mainCategory, setMainCategory] = useState("전체");
-//   const [page, setPage] = useState(1);
-//   // const [selectedButton, setSelectedButton] = useState(null);
-
-//   // const handleButtonClick = (index) => {
-//   //   setSelectedButton(index);
-//   // };
-
-//   const mainCategories = [
-//     "진지",
-//     "유머"
-//   ]
-
-//   const categories = [
-//     "전체",
-//     "패션/뷰티",
-//     "맛집/요리/음식",
-//     "경제/재테크",
-//     "취미/운동",
-//     "스포츠",
-//     "여행",
-//     "결혼",
-//     "게임",
-//     "반려동물",
-//     "가족",
-//     "취업/자격증",
-//     "일상",
-//     "기타",
-//   ];
-
-//   const [category, setCategory] = useState("전체");
-
-//   const handleCategoryClick = (category) => {
-//     setCategory(category);
-//     setPage(1);
-//     setHumour([]);
-//     console.log("카테고리---->", category);
-//   };
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await instance.get(
-//           `/postCards?maincategory=유머&category=${category}&splitNumber=4&splitPageNumber=${page}`
-//         );
-//         console.log("data------->", response.data.postCards);
-//         setHumour((prevHumour) => [...prevHumour, ...response.data.postCards]);
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     };
-//     fetchData();
-//   }, [category, page]);
-
-//   useEffect(() => {
-//     const handleScroll = () => {
-//       //try1
-//       // const ScrollHeight = document.documentElement.scrollHeight; //스크롤 하지 않았을 때의 전체 높이
-//       // const ScrollTop = document.documentElement.scrollTop; //y축 방향으로 스크롤한 거리=스크롤되어 올라간 만큼의 높이
-//       // const clientHeight = document.documentElement.clientHeight; // 눈에 보이는 만큼의 높이
-//       // const isBottom = clientHeight + ScrollTop + 1 >= ScrollHeight + "500px";
-
-//       //try2
-//       const isBottom =
-//         window.innerHeight + window.scrollY >= document.body.offsetHeight;
-//       // + "500px";
-//       if (isBottom) {
-//         setPage((prevPage) => prevPage + 1);
-//       }
-//     };
-//     window.addEventListener("scroll", handleScroll);
-//     return () => {
-//       window.removeEventListener("scroll", handleScroll);
-//     };
-//   }, []);
-
-//   //세부 카테고리를 선택해도 전체 카테고리가 호출됨.
-
-//   return (
-//     <MobileLayout>
-//       <PageWithFooterWrapper>
-//         <BoardHeader>
-//           <BoardHeaderSub>
-//             <BoardTitle>훈수게시판</BoardTitle>
-
-//             <Row>
-//               <BoardCategory>유머</BoardCategory>
-//               {/* <BoardCategory isActive>로 나중에 색깔 처리 */}
-//               <BoardCategory onClick={() => navi("/board/serious")}>
-//                 진지
-//               </BoardCategory>
-//             </Row>
-//             <CategoryWrapper>
-//               {categories.map((item, index) => (
-//                 <button
-//                   key={index}
-//                   onClick={() => handleCategoryClick(item)}
-//                   style={{ marginLeft: index === 0 ? "1.25rem" : "0.5rem" }}
-//                 >
-//                   {item}
-//                 </button>
-//               ))}
-//             </CategoryWrapper>
-//           </BoardHeaderSub>
-//         </BoardHeader>
-
-//         {humour.map((item) => (
-//           <PostCard
-//             key={item.postIdx}
-//             title={item.title}
-//             nickname={item.nickname}
-//             content={item.desc}
-//             viewCount={item.postViewCount}
-//             commentCount={item.commentCount}
-//           />
-//         ))}
-//       </PageWithFooterWrapper>
-//       <Footer />
-//     </MobileLayout>
-//   );
-// };
-
-// const CategoryWrapper = styled.div`
-//   padding: 1rem 0;
-//   width: 400px;
-//   background-color: white;
-//   overflow-x: scroll;
-//   white-space: nowrap;
-//   /* margin-left: 1rem; */
-
-//   & > button {
-//     display: inline-block;
-//     background-color: white;
-//     border: 0.1rem solid rgb(120, 120, 120);
-//     margin-right: 0.5rem;
-//     border-radius: 1.75rem;
-//     padding: 0.25rem 1rem;
-//   }
-
-//   & > button:focus {
-//     background-color: rgb(120, 120, 120);
-//     color: white;
-//     outline: none;
-//   }
-
-//   ::-webkit-scrollbar {
-//     display: none;
-//   }
-// `;
-
-// const PageWithFooterWrapper = styled.div`
-//   margin: 11rem 0 15rem 0;
-// `;
-
-// const Row = styled.div`
-//   display: flex;
-//   justify-content: space-around;
-//   width: 100%;
-// `;
-
-// const BoardHeader = styled.div`
-//   background-color: white;
-//   position: fixed;
-//   top: 0;
-//   padding-top: 0.75rem;
-//   width: 100%;
-//   max-width: 400px;
-//   font-size: 1.25rem;
-//   color: rgb(70, 70, 70);
-// `;
-
-// const BoardHeaderSub = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   /* gap: 2rem; */
-//   font-weight: bold;
-//   justify-content: space-evenly;
-// `;
-
-// const BoardCategory = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   width: 100%;
-//   padding-bottom: 0.75rem;
-//   border-bottom: 0.2rem solid rgb(70, 70, 70);
-//   color: rgb(70, 70, 70);
-//   text-decoration: none;
-
-//   &:hover {
-//     color: rgb(180, 180, 180);
-//     cursor: pointer;
-//     border-bottom: 0.1rem solid rgb(180, 180, 180); /* 새로 추가된 코드 */
-//   }
-// `;
-
-// const BoardTitle = styled.div`
-//   padding-bottom: 2rem;
-//   display: flex;
-//   justify-content: center;
-// `;
-
-// export default HumourBoard;
--- src/pages/board/SeriousBoard.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SeriousBoard = () => {
-  return <div>yet</div>;
-};
-
-export default SeriousBoard;
