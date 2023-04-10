985aa8290a037396f322c605f7aee179c98ad1bd
eh-lee <fornewstack@daum.net>
2023-04-10 11:39:09 +0900

    [Refactor] 컴파일 에러 수정 커밋

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,27 +2,29 @@ import Header from "../components/header/Header";
 import styled from "styled-components";
 import Greeting from "../components/greeting/Greeting";
 import GreetingLv from "../components/greeting/GreetingLv";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "../components/footer/Footer";
 import MobileLayout from "../layout/MobileLayout";
 // import { instance } from "../api/axios";
 import PostCardSlider from "../components/post/PostCardSlider";
 import PostCardJr from "../components/post/PostCardJr";
+import { instance } from "../api/axios";
+import PostCard from "../components/post/PostCard";
 
 const Home = () => {
   const [page, setPage] = useState(1);
   const [hotPostCards, setHotPostCards] = useState([]);
 
-  useEffect(()=>{
-    const getHotPost = async() => {
-      const response = await instance.get('/postCards/hotPostCard')
-      setHotPostCards(response.data.postCards)
-    }
-    
-    getHotPost();
-  }, [])
+  useEffect(() => {
+    const getHotPost = async () => {
+      const response = await instance.get("/postCards/hotPostCard");
+      setHotPostCards(response.data.postCards);
+    };
 
-  console.log('postCards', hotPostCards);
+    getHotPost();
+  }, []);
+
+  console.log("postCards", hotPostCards);
 
   return (
     <>
@@ -34,20 +36,22 @@ const Home = () => {
           {/* * ================ Young ============== * */}
           <PostCardSlider />
 
-        {
-          hotPostCards?.map((hotPostCard) => {
+          {hotPostCards?.map((hotPostCard) => {
             return (
               <PostCard
                 key={hotPostCard.postIdx}
-                category={hotPostCard.category} title={hotPostCard.title} nickname={hotPostCard.nickname} content={hotPostCard.desc}
-                viewCount={hotPostCard.postViewCount} commentCount={hotPostCard.commentCount}
+                category={hotPostCard.category}
+                title={hotPostCard.title}
+                nickname={hotPostCard.nickname}
+                content={hotPostCard.desc}
+                viewCount={hotPostCard.postViewCount}
+                commentCount={hotPostCard.commentCount}
               />
-            )
-          })
-        }
+            );
+          })}
 
-        {/* <h1>실시간 게시물</h1> */}
-        {/* {
+          {/* <h1>실시간 게시물</h1> */}
+          {/* {
           postCards?.map((postCard) => {
             return (
               <PostCard
@@ -58,9 +62,8 @@ const Home = () => {
             )
           })
         } */}
-        <PostCardJr />
-{/* * ================ Young ============== *    */}
-
+          <PostCardJr />
+          {/* * ================ Young ============== *    */}
         </PageWithHeaderAndFooterWrapper>
         <Footer />
       </MobileLayout>
