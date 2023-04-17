0fe661c0af83955f9d73a26dac1c192497c4e709
yeooong <yeong.kim9127@gmail.com>
2023-04-17 23:16:46 +0900

    [Feat] 카카오 로그인 연결 커밋

--- src/pages/auth/KakaoLogin.jsx
+++ src/pages/auth/KakaoLogin.jsx
@@ -1,50 +1,50 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 import MobileLayout from "../../layout/MobileLayout";
-import { cookies } from "../../api/cookies"
+import { cookies } from "../../api/cookies";
+import { useNavigate } from "react-router-dom";
 
 const KakaoLogin = () => {
   // 통신중인 페이지 => 꾸밈 / isLoading, isError
   const code = new URL(window.location.href).searchParams.get("code");
-  console.log('이거좀 찍어봐')
   console.log("카카오 인가 코드==========>", code);
+  const navi = useNavigate();
 
+  //코드 보내는 부분
+
+  //토큰 받는 부분
   useEffect(() => {
     if (code) {
       const kakao = async () => {
-        // const response = await axios.get(
-        //   `http://54.180.30.108:3002/user/kakaoLogin?code=${code}`
-        // );
-        try {
-          // const response = await axios.get(`http://54.180.30.108:3002/user/kakaoLogin?code=${code}`);
-          const response = await axios.post(
-            "http://52.78.166.176:3000/api/user/kakaoLogin/",
-            {code: code},
-            {headers : {
-              'Content-Type' : 'application/x-www-form-urlencoded;charset=utf-8'
-            }}
-          );
-          console.log("response=============>", response)
-          if (await response.headers.authorization) {
-            cookies.set("accessToken", response.headers.authorization)
-            console.log("response=============>", response)
+        const response = await axios.post(
+          `REACT_APP_SERVER_URL/user/kakaoLogin`,
+          { code: code },
+          {
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+            },
           }
-
-          } 
-          catch (error) {
-            console.log('error====>', error)
-        };
+        );
+        // if ( await response.headers.authorization) {
+        if (await response) {
+          console.log("리프레시 토큰을 찾아서…===========>", response);
+          cookies.set("access_token", response.headers.authorization);
+          cookies.set("refresh_token", response.headers.refreshtoken);
+          cookies.set("nickname", response.data.nickname);
+        }
+        return navi("/");
       };
-      // async function postKaKaoCode() {
-      //   const response = await axios.post(
-      //     "http://54.180.30.108:3002/user/kakaoLogin/",
-      //     KaKaocode
-      //   );
-      // }
-      // postKaKaoCode();
       kakao();
     }
   }, []);
+
+  // async function postKaKaoCode() {
+  //   const response = await axios.post(
+  //     "http://54.180.30.108:3002/api/user/kakaoLogin/",
+  //     KaKaocode
+  //   );
+  // }
+  // postKaKaoCode();
 
   return (
     <MobileLayout>
