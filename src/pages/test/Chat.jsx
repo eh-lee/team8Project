1f0b657cf061d1b9691345a320aa58f03adb729a
eh-lee <fornewstack@daum.net>
2023-05-01 23:21:47 +0900

    [Test] Chat test 2 commit

--- src/pages/test/Chat.jsx
+++ src/pages/test/Chat.jsx
@@ -73,8 +73,14 @@ const Chat = () => {
 
   // 서버에서 messages받아오는 코드
   useEffect(() => {
-    socket.on("message", (message) => {
-      setMessages((messages) => [...messages, message]);
+    // socket.on("message", (message) => {
+    //   console.log("message");
+    // });
+
+    socket.on("message", ({ user, text }) => {
+      // console.log("userseuser---------------->", user);
+      //  console.log("tettxtetxt---------------->", text);
+      setMessages((messages) => [...messages, { user, text }]);
     });
 
     // socket.on("roomData", ({ room, users, messages }) => {
@@ -95,7 +101,9 @@ const Chat = () => {
     event.preventDefault();
 
     if (message) {
-      socket.emit("sendMessage", { message }, () => setMessage(""));
+      // socket.emit("sendMessage", { message }, () => setMessage(""));
+      socket.emit("message", { room, msg: message });
+      setMessage("");
     }
   };
 
