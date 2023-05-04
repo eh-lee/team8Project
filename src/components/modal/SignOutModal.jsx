293e41747695994208f20bd3a7bfc9e71791744c
eh-lee <fornewstack@daum.net>
2023-05-05 00:30:29 +0900

    [Fix] 회원탈퇴 API 수정

--- src/components/modal/SignOutModal.jsx
+++ src/components/modal/SignOutModal.jsx
@@ -2,12 +2,12 @@ import React, { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { cookies } from "../../api/cookies";
 import { instanceWithAuth } from "../../api/axios";
-import * as St from "./SignOutModal.style"
+import * as St from "./SignOutModal.style";
 
 const SignOutModal = ({ open, close }) => {
   const navi = useNavigate();
   const modalRef = useRef();
-  const email = cookies.get('email');
+  const email = cookies.get("email");
 
   useEffect(() => {
     const handleClickOutside = (event) => {
@@ -25,15 +25,14 @@ const SignOutModal = ({ open, close }) => {
 
   const signout = async () => {
     try {
-        const res = await instanceWithAuth.delete("/user/delete", { "email" : email })
-        // alert(res.data.msg)
-        navi("/");
-        // navi("/onboarding");
-        
+      const res = await instanceWithAuth.delete("/user/leave", {
+        email: email,
+      });
+      navi("/");
     } catch (err) {
-        console.error(err);
-    } 
-    
+      console.error(err);
+    }
+
     // 왜 안 되나 체크 4/21 17:44
     // [Refactor] 인터셉터로 처리하게
     cookies.remove("access_token");
@@ -43,28 +42,20 @@ const SignOutModal = ({ open, close }) => {
   };
 
   return open ? (
-    (
-        <St.FooBG>
-          <St.ChatEndWrap ref={modalRef}>
-            <St.ChatEndList column medium>
-              <St.ButtonText Large>경고</St.ButtonText>
-              <St.ChatEndInfo>
-                정말 계정을 삭제하시겠습니까?
-              </St.ChatEndInfo>
-            </St.ChatEndList>
-            <St.ChatEndList
-              onClick={signout}
-              isLogout
-              cursor
-            >
-              <St.ButtonText>회원탈퇴</St.ButtonText>
-            </St.ChatEndList>
-            <St.ChatEndList onClick={close} topMargin cursor>
-              <St.ButtonText>취소</St.ButtonText>
-            </St.ChatEndList>
-          </St.ChatEndWrap>
-        </St.FooBG>
-      )
+    <St.FooBG>
+      <St.ChatEndWrap ref={modalRef}>
+        <St.ChatEndList column medium>
+          <St.ButtonText Large>경고</St.ButtonText>
+          <St.ChatEndInfo>정말 계정을 삭제하시겠습니까?</St.ChatEndInfo>
+        </St.ChatEndList>
+        <St.ChatEndList onClick={signout} isLogout cursor>
+          <St.ButtonText>회원탈퇴</St.ButtonText>
+        </St.ChatEndList>
+        <St.ChatEndList onClick={close} topMargin cursor>
+          <St.ButtonText>취소</St.ButtonText>
+        </St.ChatEndList>
+      </St.ChatEndWrap>
+    </St.FooBG>
   ) : null;
 };
 
