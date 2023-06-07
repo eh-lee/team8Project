e7b36872c872f9d1a8900caf0bac2f55aaa8c44f
eh-lee <fornewstack@daum.net>
2023-06-08 00:39:55 +0900

    [Feat] XSS 공격 대응을 위한 sanitizeInput() 유틸 제작 commit

---
+++ src/components/util/sanitizeInput.jsx
@@ -0,0 +1,15 @@
+const sanitizeInput = (input) => {
+  const regex = /[&<>"'/]/g;
+  const entities = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&#39;",
+    "/": "&#x2F;",
+  };
+
+  return input.replace(regex, (match) => entities[match]);
+};
+
+export default sanitizeInput();
