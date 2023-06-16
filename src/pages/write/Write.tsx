f3410099bba919ec625eb3a897d1a55b700c3c69
eh-lee <fornewstack@daum.net>
2023-06-16 23:46:29 +0900

    [Refactor] Write.jsx -> tsx 전환 commit

--- src/pages/write/Write.tsx
+++ src/pages/write/Write.tsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from "react";
 import WriteForm from "../../components/form/WriteForm";
 import FalseGuard from "../../components/hook/guard/FalseGuard";
-import MobileLayout from "../../layout/MobileLayout.tsx";
+import MobileLayout from "../../layout/MobileLayout";
 import { Loading } from "../../components/onboarding/Loading";
 
-const Write = () => {
+const Write: React.FC = () => {
   FalseGuard();
   useEffect(() => {
     document.title = "훈수 — 글쓰기";
   }, []);
 
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState<boolean>(false);
 
   return (
     <>
