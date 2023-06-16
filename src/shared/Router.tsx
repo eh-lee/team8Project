9a2825e7c69ae465c1a6f863d920170e276d600d
eh-lee <fornewstack@daum.net>
2023-06-16 23:48:55 +0900

    [Refactor] Router.jsx -> tsx 전환 commit

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import Home from '../pages/home/Home'
-import Chat from "../pages/test/Chat"
-import Board from '../pages/board/Board.tsx'
-import Write from '../pages/write/Write'
-import MyPage from '../pages/user/MyPage'
-import Search from '../pages/search/Search'
-import Battle from '../pages/battle/RealTimeBattle'
-import EditPost from '../components/post/EditPost'
-import LogInPage from '../pages/auth/LogInPage'
-import KakaoLogin from '../pages/auth/KakaoLogin'
-import SignUpPage from '../pages/auth/SignUpPage.tsx'
-import DoneBattle from '../pages/battle/DoneBattle'
-import DetailPost from '../pages/detailPost/DetailPost'
-import OnBoarding from '../pages/onboarding/OnBoarding'
-import ProfileEditPage from '../pages/user/ProfileEditPage'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import React from "react";
+import Home from "../pages/home/Home";
+import Chat from "../pages/test/Chat";
+import Board from "../pages/board/Board.tsx";
+import Write from "../pages/write/Write";
+import MyPage from "../pages/user/MyPage";
+import Search from "../pages/search/Search";
+import Battle from "../pages/battle/RealTimeBattle";
+import EditPost from "../components/post/EditPost";
+import LogInPage from "../pages/auth/LogInPage";
+import KakaoLogin from "../pages/auth/KakaoLogin";
+import SignUpPage from "../pages/auth/SignUpPage.tsx";
+import DoneBattle from "../pages/battle/DoneBattle";
+import DetailPost from "../pages/detailPost/DetailPost.tsx";
+import OnBoarding from "../pages/onboarding/OnBoarding";
+import ProfileEditPage from "../pages/user/ProfileEditPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-const Router = () => {
+const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
@@ -37,7 +37,7 @@ const Router = () => {
         <Route path="/search" element={<Search />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default Router
+export default Router;
