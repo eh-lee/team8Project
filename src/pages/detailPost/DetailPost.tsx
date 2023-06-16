6fe507219ad79d7908b700504d78e9e03f4ac08c
eh-lee <fornewstack@daum.net>
2023-06-16 23:38:47 +0900

    [Refactor] DetailPost.jsx -> tsx 전환 commit

--- src/pages/detailPost/DetailPost.tsx
+++ src/pages/detailPost/DetailPost.tsx
@@ -1,20 +1,33 @@
 import React, { useEffect, useState } from "react";
 import DetailPoll from "../../components/detail/DetailPoll";
 import DetailHeader from "../../components/header/DetailHeader";
-import MobileLayout from "../../layout/MobileLayout.tsx";
+import MobileLayout from "../../layout/MobileLayout";
 import DetailPostContents from "../../components/detail/DetailPostContents";
 import DetailPostCommentsList from "../../components/detail/DetailPostCommentsList";
 import { useParams } from "react-router-dom";
 import { instance, instanceWithAuth } from "../../api/axios";
 
-const DetailPost = () => {
-  const { postIdx } = useParams();
-  const [detailPostCat, setDetailPostCat] = useState([]);
-  const [writerNickname, setWriterNickname] = useState([]);
+interface DetailPostProps {
+  postIdx: string;
+  [key: string]: string | undefined;
+}
+
+interface DetailPostData {
+  post: {
+    nickname: string;
+  };
+}
+
+const DetailPost: React.FC = () => {
+  const { postIdx } = useParams<DetailPostProps>();
+  const [detailPostCat, setDetailPostCat] = useState<any[]>([]);
+  const [writerNickname, setWriterNickname] = useState<string>("");
 
   useEffect(() => {
     const getDetailPost = async () => {
-      const { data } = await instanceWithAuth.get(`/postCards/post/${postIdx}`);
+      const { data } = await instanceWithAuth.get<DetailPostData>(
+        `/postCards/post/${postIdx}`
+      );
       setWriterNickname(data.post.nickname);
     };
     getDetailPost();
@@ -22,7 +35,7 @@ const DetailPost = () => {
 
   useEffect(() => {
     const getDetailPostCat = async () => {
-      const { data } = await instance.get(
+      const { data } = await instance.get<any[]>(
         `/postCards/post/category/${postIdx}`
       );
       setDetailPostCat(data);
@@ -30,11 +43,11 @@ const DetailPost = () => {
     getDetailPostCat();
   }, [postIdx]);
 
-  const [detailPoll, setDetailPoll] = useState({});
+  const [detailPoll, setDetailPoll] = useState<any>({});
 
   useEffect(() => {
     const getDetailPoll = async () => {
-      const { data } = await instanceWithAuth.get(
+      const { data } = await instanceWithAuth.get<any>(
         `/postCards/post/contents/${postIdx}`
       );
       setDetailPoll(data.contents);
